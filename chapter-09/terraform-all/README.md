e459eadb5670e239b70369fe9853df0ff6cb91c4
Han Jiwoo <jiw01@naver.com>
2024-06-22 17:11:13 +0900

    Add practice code for chapter 09

---
+++ chapter-09/terraform-all/README.md
@@ -0,0 +1,2 @@
+# Overview
+Chapter 09 실습을 위해 [terraform-aws-tfc-workflow](https://github.com/terraform101/terraform-aws-tfc-workflow) 레포지토리를 fork 하여 사용
---
+++ chapter-09/terraform-all/variables.tf
@@ -0,0 +1,64 @@
+variable "prefix" {
+  description = "This prefix will be included in the name of most resources."
+}
+
+variable "region" {
+  description = "The region where the resources are created."
+  default     = "ap-northeast-2"
+}
+
+variable "address_space" {
+  description = "The address space that is used by the virtual network. You can supply more than one address space. Changing this forces a new resource to be created."
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_prefix" {
+  description = "The address prefix to use for the subnet."
+  default     = "10.0.10.0/24"
+}
+
+variable "instance_type" {
+  description = "Specifies the AWS instance type."
+  default     = "t3.micro"
+}
+
+variable "height" {
+  default     = "400"
+  description = "Image height in pixels."
+}
+
+variable "width" {
+  default     = "600"
+  description = "Image width in pixels."
+}
+
+variable "placeholder" {
+  default     = "placekitten.com"
+  description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
+}
+
+variable "environment" {
+  type        = string
+  description = "Define infrastructure’s environment"
+  default     = "dev"
+  validation {
+    condition     = contains(["dev", "qa", "prod"], var.environment)
+    error_message = "The environment value must be dev, qa, or prod."
+  }
+}
+
+variable "ec2_count" {
+  type = number
+  default = 1
+  validation {
+    condition = var.ec2_count >= 1
+    error_message = "ec2_count must not be lower than 0"
+  }
+}
+
+variable "public_access_cidr" {
+  type = list(string)
+  default = [
+    "0.0.0.0/0"
+  ]
+}
