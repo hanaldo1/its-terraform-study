89834449869d7c80d52d0683804401e33253c8c2
Han Jiwoo <jiw01@naver.com>
2024-06-23 20:58:25 +0900

    modify

--- chapter-09/module-management/root/main.tf
+++ chapter-09/module-management/root/main.tf
@@ -10,7 +10,14 @@ terraform {
 provider "aws" {}
 
 module "vpc" {
-  source = "../vpc"
+#   source = "../vpc"
+
+  # Github
+  # source = "github.com/hanaldo1/its-terraform-study//chapter-09//module-management//vpc?ref=v0.0.1"
+
+  # TFC Registry
+  source  = "app.terraform.io/hanaldo-tf/module/aws"
+  version = "0.0.1"
 
   prefix = "module1"
   vpc_cidr = "10.0.0.0/16"
